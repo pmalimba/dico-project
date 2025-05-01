8ccf3a18e93cb80ed920dc25d02f6a7739556659
pmalimba <pmalimba@yahoo.com>
2025-05-01 14:24:08 +0200

    Added api documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,6 +13,7 @@ export default function Dictionary() {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
 
+    //documentation: https://dictionaryapi/dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
